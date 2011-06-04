ea65c89cac7c144438653ee06eb4a90d700a0fdb
Mohamed Abd El Hakim <7akimz@gmail.com>
2011-06-05 00:35:33 +0300

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brainstormer::Application.config.secret_token = 'b4689ae46e57f4af7f93ffff21e57a3c100160909ad7d89257e964b9ccef71a195c1d2bd19887d11a7f1b9c921aff3a5c9a9696740c72db0d3b4eab8bf1d80c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Brainstormer::Application.config.session_store :cookie_store, :key => '_brainstormer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Brainstormer::Application.config.session_store :active_record_store
